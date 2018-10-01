a37a8a0007fe648278b88f9990ab53a73e58e49f
kosteng <2267672@mail.ru>
2018-10-01 14:17:46 +0300

    дописал

--- lesson8/lesson8/Program.cs
+++ lesson8/lesson8/Program.cs
@@ -10,7 +10,7 @@ namespace lesson8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("  ");
+            Console.WriteLine(" дописал");
             Console.ReadKey();
         }
     }
